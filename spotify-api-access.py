d23bc93a4e0f070e8109848ec13d319f31e3051b
leoflewis <fleonardolewis@gmail.com>
2021-06-04 16:32:56 -0500

    whitespace

--- spotify-api-access.py
+++ spotify-api-access.py
@@ -15,6 +15,7 @@ results = spotify.search(q='artist:' + name, type='artist')
 artist_id = results['artists']['items'][0]['id']
 str_id = 'spotify:artist:'+artist_id
 artist = spotify.artist(str_id)
+
 print('Artist: ' + artist['name'])
 print()
 print('Artist top 10:')
@@ -22,11 +23,13 @@ artist_results = spotify.artist_top_tracks(str_id)
 for track in artist_results['tracks']:
 	print('-' + track['name'])
 print()
+
 print('Artist albums:')
 artist_albums = spotify.artist_albums(str_id, album_type='album', country='US', offset=0, limit=50)
 for album in artist_albums['items']:
 	print('-' + album['name'])
 print()
+
 related_artists = spotify.artist_related_artists(str_id)
 print('Related artists:')
 for artist in related_artists['artists']:
